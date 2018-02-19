b3ac3451471ca9d0e74115ee711ecae55bc92029
ZjanPreijde <j.preijde2@upcmail.nl>
2018-02-19 09:40:48 +0100

    Saving a broken api

--- routes/games.js
+++ routes/games.js
@@ -39,7 +39,7 @@ module.exports = io => {
           symbol: "X",
           playerSquares: []
         }],
-        gameSquares: []
+        gameSquares: ["", "", "", "", "", "", "", "", ""]
       }
 
       Game.create(newGame)
